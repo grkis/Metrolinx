2505f9cd0c2d6e5492859b47bda0e08d015ac057
grkis <114247048+grkis@users.noreply.github.com>
2023-04-14 12:01:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,5 +33,10 @@ function EButton() {
 }
 
 function OButton() {
+    window.location.href = "https://fleming.maps.arcgis.com/apps/mapviewer/index.html?webmap=18527f59168945de8711e593ce8c2a81";
+  }
+
+  function openWebsiteInNewTab(event) {
+    event.preventDefault();
     window.open("https://fleming.maps.arcgis.com/apps/mapviewer/index.html?webmap=18527f59168945de8711e593ce8c2a81", "_blank");
-}
+  }
